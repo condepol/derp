9507c149d51f88317823c6d36da4617752854108
Antoine Méric <mericantoi@eisti.eu>
2014-01-18 12:55:58 +0100

    trouvé \!

--- test/python/programme.py
+++ test/python/programme.py
@@ -2,13 +2,32 @@
 # coding: utf-8
 
 class A():
-  def __init__(self,par='azer'):
-    print ('Initializing a A with parameter {}'.format(par))
+  def __init__(self,*par):
+
+    try:
+      self.forA = par[0]['forA']
+    except Exception as e:
+      print(e)
+      self.forA = 'default A :/'
+
+    print('Initializing a A with parameter {}'.format(self.forA))
 
 class B(A):
-  def __init__(self,par='tyui'):
-    super().__init__(par)
-    print ('Initializing a E with parameter {}'.format(par))
+  def __init__(self,*par):
+    super().__init__(*par)
+
+    try:
+      self.forB = par[0]['forB']
+    except Exception as e:
+      print(e)
+      self.forB = 'default B :/'
+
+    print('Initializing a E with parameter {}'.format(self.forB))
 
 if __name__ == ('__main__'):
-  print(B('derp'))
+  b = B({
+    'forB':'thisisforB',
+    'forA':'information_for_A'})
+
+  print('Initialized in B.__init__ : {}'.format(b.forB))
+  print('Initialized in A.__init__ : {}'.format(b.forA))
