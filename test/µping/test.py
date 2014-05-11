8550ac25d28d6319e73cd0fcefad90e1367f9cb4
Antoine Méric <mericantoi@eisti.eu>
2014-05-11 17:51:28 +0200

    correct revision of checksum

--- test/µping/test.py
+++ test/µping/test.py
@@ -1,14 +1,28 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # coding: utf-8
 
 import os,select,socket,struct,sys,time
 import random
 
-def checksum_mini(source_string):
-  return struct.unpack('<H',struct.pack('>H',65535^(
-    (sum(struct.unpack('H',source_string[i:i+2])[0]for i in range(0,len(source_string),2))%0x10000) +
-    (sum(struct.unpack('H',source_string[i:i+2])[0]for i in range(0,len(source_string),2)) >> 16))
-    ))[0]
+def check(somme,mine):
+  try:
+    assert(somme == mine)
+  except AssertionError:
+    print(' '.join(['{:04x}'.format(i) for i in [somme,mine]]))
+    sys.exit(0)
+
+
+def checksum_mini(data):
+    # accumulate shorts
+    mine=sum(data[i]+(data[i+1]<<8) for i in range(0,(len(data)|1)-1,2))
+    # add last odd byte
+    if len(data)&1: mine += data[-1]
+    # shift carries
+    while mine>>16: mine = (mine>>16) + (mine%65536)
+    # swap bytes and xor
+    return (((mine&255)<<8)+(mine>>8)) ^ 65535
+
+
 
 
 def checksum_maxi(source_string):
@@ -17,14 +31,14 @@ def checksum_maxi(source_string):
     to suggest that it gives the same answers as in_cksum in ping.c
     """
     sum = 0
-    count_to = (len(source_string) / 2) * 2
-    for count in xrange(0, count_to, 2):
-        this = ord(source_string[count + 1]) * 256 + ord(source_string[count])
+    count_to = (len(source_string) // 2) * 2
+    for count in range(0, count_to, 2):
+        this = source_string[count + 1] * 256 + source_string[count]
         sum = sum + this
         sum = sum & 0xffffffff # Necessary?
 
     if count_to < len(source_string):
-        sum = sum + ord(source_string[len(source_string) - 1])
+        sum = sum + source_string[len(source_string) - 1]
         sum = sum & 0xffffffff # Necessary?
 
     sum = (sum >> 16) + (sum & 0xffff)
@@ -39,6 +53,10 @@ def checksum_maxi(source_string):
 
 
 if __name__ == '__main__':
-  for i in xrange(555):
-    vector = ''.join([chr(random.randrange(256)) for i in xrange((random.randrange(400)|1)+1)])
-    assert(checksum_mini(vector) == checksum_maxi(vector))
+  try:
+    for i in range(2000):
+      vector = bytes([random.randrange(256) for i in range(random.randrange(400))])
+      assert(checksum_mini(vector) == checksum_maxi(vector))
+    print('\x1b[32mOk\x1b[0m')
+  except AssertionError:
+    print('\x1b[31mFail\x1b[0m')
