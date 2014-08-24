8663d4d8443ad11756c65b2b1cad2afde670c293
qolund <rms@gnu.org>
2014-08-24 16:06:12 +0200

    stop childs, be rms.

--- pathbin/pls_dont_burn.sh
+++ pathbin/pls_dont_burn.sh
@@ -7,11 +7,13 @@ result=`kill -0 ${target_pid}`
 while ${result};
 do
   echo "Stopping process ${target_pid} .."
+  for fpid in $(ps -o pid --ppid=$pid) ; do kill -STOP $fpid ; done
   kill -STOP ${target_pid}
   echo "Refreshing .."
   refre.sh $temp_max
   echo "Working for 30s";
   kill -CONT ${target_pid}
+  for fpid in $(ps -o pid --ppid=$pid) ; do kill -CONT $fpid ; done
   sleep 20
   result=`kill -0 ${target_pid}`
 done
