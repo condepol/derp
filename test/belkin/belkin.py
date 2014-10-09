1dff85391e370616513bee2e4cd3b2f1453d9721
qolund <0.le.phare.ouest@gmail.com>
2014-10-09 11:24:07 +0200

    python3, readability

--- test/belkin/belkin.py
+++ test/belkin/belkin.py
@@ -1,2 +1,20 @@
-import struct as s;
-print("".join(["944626378ace9bdf"[int(i,16)]for i in"".join([s.pack(">I",1+s.unpack(">I","08863B053A41"[-8:].decode('hex'))[0]).encode('hex')[int(u)]for u in"51274063"])]));
+#!/usr/bin/env python3
+import sys
+import codecs
+import struct
+# hardcoded tables
+table = "944626378ace9bdf"
+order = [5,1,2,7,4,0,6,3]
+# read mac address
+mac = "08863B053A41" if len(sys.argv) == 1 else sys.argv[1]
+# convert to bytes
+code = codecs.decode(mac[-8:],'hex')
+# add 1
+code = struct.pack('>I',1+struct.unpack('>I',code)[0])
+# transform to hex
+code = str(codecs.encode(code,'hex'),'ascii')
+# extract positions
+code = [int(code[u],16) for u in order]
+# extract code letters
+code = [table[u] for u in code]
+print(''.join(code))
